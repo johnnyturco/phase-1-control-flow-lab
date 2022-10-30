31fcc1371aa807eb5dff65d18a147f38d2260ad2
Johnny <johnnyturco@me.com>
2022-10-30 12:25:30 -0600

    commit tree a3ccd549a180bfcfd91538191e454df579965d27

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if(someValue <= 400){
+    return 'This one is on me!';
+  } else if(someValue > 400 && someValue < 2000) {
+    return 'That will be twenty bucks.';
+  } else if(someValue > 2500) {
+    return 'No can do.';
+  } else {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
